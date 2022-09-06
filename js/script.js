1892b889b47daee559bdcc56ee6b59290ffe72db
lani vv <leonorehall@gmail.com>
2022-09-06 12:40:57 -0400

    this is a commit with problems that are going to be solved in time

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-//selecting the div that handles the profile info
-const profile = document.querySelector(".overview")
-const username = "LaniV-V"
+const profile = document.querySelector(".overview");
+const username = "LaniV-V";
+const repoList = document.querySelector(".repolist");
 
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -8,10 +8,10 @@ const gitUserInfo = async function () {
    // console.log(data);
    displayUserInfo(data);
 };
+
 gitUserInfo();
 
-
-const displayUserInfo = function(data) {
+const displayUserInfo = function (data) {
     const div = document.createElement("div");
     div.classList.add("user-info");
     div.innerHTML = `
@@ -26,4 +26,28 @@ const displayUserInfo = function(data) {
     </div>
     `;
     profile.append(div);
+    gitRepos();
 };
+const gitRepos = async function () {
+      const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+      const repoData = await fetchRepos.json();
+      console.log(repoData);
+      displayRepos(repoData);
+  };
+
+  const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add ("repo"); 
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+  };
+  
+
+
+
+
+
+
+
